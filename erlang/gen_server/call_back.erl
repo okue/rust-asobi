6f5059ff5b5bc9f97b1a6bd00332d6aade28c9bc
okue <nogideca@gmail.com>
2018-07-13 01:22:24 +0900

    delete no-necessary files

--- erlang/gen_server/call_back.erl
+++
@@ -1,5 +0,0 @@
--module(call_back).
--export([handle_call/1]).
-
-handle_call(Request) ->
-  io:format("print ~p~n",[Request]).
--- erlang/gen_server/call_back2.erl
+++
@@ -1,5 +0,0 @@
--module(call_back2).
--export([handle_call/1]).
-
-handle_call(Request) ->
-  io:format("PRINT ~p~n",[Request]).
