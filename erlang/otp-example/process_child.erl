a13f87f279d25fb15541fa34df391a44d67659dc
okue <nogideca@gmail.com>
2018-08-08 10:43:34 +0900

    update process_child.erl

--- erlang/otp-example/process_child.erl
+++ erlang/otp-example/process_child.erl
@@ -5,70 +5,59 @@
 %%          API functions                                                    %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%% 前提:
-%% スーパバイザの親は, アプリケーションまたはスーパバイザ.
-%% スーパバイザとリンクしているのは, スーパバイザの直接の子供のみ.
-%% 子供以外のプロセスが, スーパバイザに対してリンクはできない.
-%%
-%% 制限:
+%% XXX:
+%% スーパバイザから下へ辿るときは, supervisor:which_children/1で取得できる
+%% プロセスのリストを使う.
 %% '$initial_call'が, supervisorでなくsupervisor_bridgeの場合は,
 %% スーパバイザの子供を追えない.
+%%
+%% 普通のプロセスから下へ辿るときは, erlang:process_info/2で取得できる
+%% リンクしているプロセスのリストを使う.
+%% ポートからのリンクは, erlang:port_info/2で取得する.
 %%
 
 %%
 %% SupRefList must be list of supervisor pid or name.
 %%
 search(SupRefList) when is_list(SupRefList) ->
-  Pids = lists:flatmap(fun(SupRef) -> search(supervisor, SupRef) end, SupRefList),
-  lists:sort(Pids).
-
+  Pids =
+    lists:flatmap(
+      fun(SupRef) ->
+        SupPid = to_pid(SupRef),
+        search(supervisor, SupPid, [SupPid])
+      end,
+      SupRefList),
+  lists:usort(Pids).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%        Internal functions                                                 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-search(supervisor, undefined) -> [];
+search(_Type, undefined, Acc) -> Acc;
 
-search(supervisor, SupRef) when is_pid(SupRef) or is_atom(SupRef) ->
-  Ref = to_pid(SupRef),
-  %% 仮定:
-  %%  任意のスーパバイザの子供木t_1, t_2について,
-  %%  t_1内のプロセスとt_2内のプロセスの間にリンクはない
-  Children = lists:flatmap(
-    fun
-      ({_Name, Pid, supervisor, _}) ->
-        % Not include Ref in this list
-        search(supervisor, Pid);
-      ({_Name, Pid, worker, _}) ->
-        % Not include Ref in this list
-        search(worker, Pid, [Ref]) -- [Ref]
+search(supervisor, SupPid, Acc) when is_pid(SupPid) ->
+  lists:foldl(
+    fun({_Name, Pid, Type, _}, Acc2) ->
+      search(Type, Pid, Acc2)
     end,
-    supervisor:which_children(Ref)),
-  % Append Ref to the result
-  lists:umerge([Ref], Children).
+    lists:umerge([SupPid], Acc),
+    supervisor:which_children(SupPid));
 
-search(worker, undefined, Acc) -> Acc;
-
-search(worker, Ref, Acc) when (is_pid(Ref) or is_port(Ref)) and is_list(Acc) ->
+search(worker, Pid, Acc) when (is_pid(Pid) or is_port(Pid)) and is_list(Acc) ->
   {links, AllLinks} =
-    case is_pid(Ref) of
-      true  -> erlang:process_info(Ref, links);
-      false -> erlang:port_info(Ref, links)
+    case is_pid(Pid) of
+      true  -> erlang:process_info(Pid, links);
+      false -> erlang:port_info(Pid, links)
     end,
-  %% Refがリンクしているプロセスから, Accを除く
+  %% Pidがリンクしているプロセスから, Accを除く
   Links = lists:sort(AllLinks) -- Acc,
-  % io:format("~p --> ~p ~p~n", [Ref, Acc, Links]),
-  MergedAcc = lists:umerge([Ref], Acc),
-  case Links of
-    [] -> MergedAcc;
-    _  ->
-      lists:foldl(
-        fun(Link, Acc2) ->
-          search(worker,
-                 Link,
-                 lists:umerge(MergedAcc, Acc2))
-        end, [], Links)
-  end.
+  io:format("~p --> ~p ~p~n", [Pid, Acc, Links]),
+  lists:foldl(
+    fun(Link, Acc2) ->
+      search(worker, Link, Acc2)
+    end,
+    lists:umerge([Pid], Acc),
+    Links).
 
 to_pid(Ref) when is_pid(Ref) -> Ref;
 to_pid(Ref) when is_atom(Ref) -> whereis(Ref).
